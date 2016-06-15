756c5396b24ec0d3b07744fd7007f42130a54255
Guilherme de Lemos <guilherme.eti@gmail.com>
2016-06-15 13:55:27 -0300

    Exemplo de projeto java web usando gradle e travis-ci

---
+++ Java/exemplos/JavaWebGradle/README.md
@@ -0,0 +1,10 @@
+[![Build Status](https://travis-ci.org/guilhermedelemos/octo-pet.svg?branch=develop)](https://travis-ci.org/guilhermedelemos/octo-pet)
+
+# octo-pet
+pet project
+
+Dependências
+Bootstrap Version 3.3.6
+jQuery Version 2.2.3
+Font Awesome Version 4.6.2
+AdminLTE Version 2.3.3
---
+++ Java/exemplos/JavaWebGradle/src/main/java/com/estadodaarte/PrincipalServlet.java
@@ -0,0 +1,51 @@
+package com.estadodaarte;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(name = "PrincipalServlet", urlPatterns = {"/"})
+public class PrincipalServlet extends HttpServlet {
+
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = response.getWriter();
+        try {
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet PrincipalServlet</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Servlet PrincipalServlet</h1>");
+            out.println("</body>");
+            out.println("</html>");
+        } finally {
+            out.close();
+        }
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }
+
+}
+

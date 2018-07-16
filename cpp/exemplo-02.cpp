e25421208fb7ab3cff8afa948141936f85171ef2
Guilherme de Lemos <guilherme.eti@gmail.com>
2018-07-16 18:11:34 -0300

    Exemplos C++

---
+++ cpp/exemplo-02.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a;
+    int b;
+    int c;
+
+    a = 1;
+    b = 3;
+    c = a + b;
+
+    cout << "Result: " << c << endl;
+    return 0;
+}
